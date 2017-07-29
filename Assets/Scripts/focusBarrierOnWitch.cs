65001b8c821244b0e60601ade0436d4daad150ea
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2017-07-29 19:49:35 +0200

    Created and updated map

---
+++ Assets/Scripts/focusBarrierOnWitch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class focusBarrierOnWitch : MonoBehaviour {
+
+
+	GameObject witch;
+	// Use this for initialization
+	void Start () {
+		witch = GameObject.FindGameObjectWithTag ("Witch");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = witch.transform.position;
+	}
+}
