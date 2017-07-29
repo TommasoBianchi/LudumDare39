ea9fb1ba085e69ad12da49dfc9bc225753c1c867
Unknown <jack-f@hotmail.it>
2017-07-30 00:08:26 +0200

    Animations and animator for the player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour {
 
 	public float hp {get; set;}
 	public float maxHp {get; private set;}
+    public Animator PlayerAnimator;
 
 	[SerializeField]
 	private float baseDamage;
@@ -37,14 +38,44 @@ public class Player : MonoBehaviour {
 		
 	void wasdMovement(){
 
-		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0).normalized;
 		transform.position += move * baseSpeed * Time.deltaTime;
-		
-	}
+
+        if(Mathf.Abs(move.y)>Mathf.Abs(move.x) && move.y>move.x)
+        {
+            PlayerAnimator.SetInteger("MoveDir", 0);
+            PlayerAnimator.SetBool("Moving", true);
+        }
+
+        else if (Mathf.Abs(move.x) > Mathf.Abs(move.y) && move.x > move.y)
+        {
+            PlayerAnimator.SetInteger("MoveDir", 1);
+            PlayerAnimator.SetBool("Moving", true);
+        }
+
+        else if (Mathf.Abs(move.x) > Mathf.Abs(move.y) && move.y > move.x)
+        {
+            PlayerAnimator.SetInteger("MoveDir", 3);
+            PlayerAnimator.SetBool("Moving", true);
+        }
+
+        else if (Mathf.Abs(move.y) > Mathf.Abs(move.x) && move.x > move.y)
+        {
+            PlayerAnimator.SetInteger("MoveDir", 2);
+            PlayerAnimator.SetBool("Moving", true);
+        }
+
+        else {
+            PlayerAnimator.SetBool("Moving", false);
+            PlayerAnimator.SetInteger("MoveDir", 5);
+        }
+
+    }
 
 	void damage(){
 		if (Input.GetMouseButton (0)) {
 			Debug.Log ("DAMAGE");
+            PlayerAnimator.Play("PlayerAttackRight");
 		}
 	}
 }
