9fdf90ed2bcdd00d8c2d42fc660100131bfdc1ff
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2017-07-30 19:29:42 +0200

    Drag and drop

---
+++ Assets/Scripts/DraggableElement.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DraggableElement : MonoBehaviour {
+
+    private Vector3 startPos;
+    private Vector3 offset;
+    private bool isDragging = false;
+    private DragManager dragManager;
+
+	void Start () 
+    {
+        startPos = transform.position;
+        dragManager = FindObjectOfType<DragManager>();
+	}
+	
+	void Update () 
+    {
+        if (isDragging)
+        {
+            if (!Input.GetMouseButton(0))
+            {
+                isDragging = false;
+                IDragDestination destination = dragManager.GetDragDestinationUnderMouse();
+                if (destination != null && destination.CanRelease(this))
+                {
+                    destination.Release(this);
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    transform.position = startPos;
+                }
+                return;
+            }
+
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+            pos.z = 0;
+            transform.position = pos;
+        }
+	}
+
+    public void StartDrag(Vector3 clickPosition)
+    {
+        offset = transform.position - clickPosition;
+        isDragging = true;
+    }
+}
---
+++ Assets/Scripts/IDragDestination.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDragDestination {
+
+    bool CanRelease(DraggableElement element);
+    void Release(DraggableElement element);
+}
